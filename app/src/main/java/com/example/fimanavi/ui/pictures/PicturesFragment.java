2eb29046ac66c7c60080ca4a7ab5c5140486d613
PhuocPro <Phuoc875@gmail.com>
2019-11-15 08:02:03 +0700

    Chang location of TextAdapter

--- app/src/main/java/com/example/fimanavi/ui/pictures/PicturesFragment.java
+++ app/src/main/java/com/example/fimanavi/ui/pictures/PicturesFragment.java
@@ -31,6 +31,7 @@ import com.example.fimanavi.Common;
 import com.example.fimanavi.Constant;
 import com.example.fimanavi.FileUtils;
 import com.example.fimanavi.R;
+import com.example.fimanavi.TextAdapter;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -339,76 +340,6 @@ public class PicturesFragment extends Fragment {
             isFileManagerInitialized = true;
         } else {
             refreshButton.callOnClick();
-        }
-    }
-
-
-    class TextAdapter extends BaseAdapter {
-        private List<String> data = new ArrayList<>();
-        private boolean[] selection;
-
-        public void setData(List<String> data) {
-            if (data != null) {
-                this.data.clear();
-                if (data.size() > 0) {
-                    this.data.addAll(data);
-                }
-                notifyDataSetChanged();
-            }
-        }
-
-        void setSelection(boolean[] selection) {
-            if (selection != null) {
-                this.selection = new boolean[selection.length];
-                for (int i = 0; i < selection.length; i++) {
-                    this.selection[i] = selection[i];
-                }
-                notifyDataSetChanged();
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return data.size();
-        }
-
-        @Override
-        public String getItem(int position) {
-            return data.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null) {
-                convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
-                convertView.setTag(new ViewHolder((TextView) convertView.findViewById(R.id.textItem)));
-            }
-
-            // Holder when long press
-            ViewHolder holder = (ViewHolder) convertView.getTag();
-            final String item = getItem(position);
-            holder.info.setText(item.substring(item.lastIndexOf('/') + 1));
-            if (selection != null) {
-                if (selection[position]) {
-                    holder.info.setBackgroundColor(Color.argb(100, 202, 221, 237)); // Màu của Item khi được select
-                } else {
-                    holder.info.setBackgroundColor(Color.WHITE); // Màu của Item khi bỏ select
-                }
-            }
-            return convertView;
-        }
-
-        class ViewHolder {
-            TextView info;
-
-            ViewHolder(TextView info) {
-                this.info = info;
-            }
         }
     }
 
